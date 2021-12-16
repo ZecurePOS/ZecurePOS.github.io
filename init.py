38ba4b558670f747f4f0d83cfceacfcf183b17da
RadoBabo <dominikradojevic@web.de>
2021-12-16 11:46:02 +0100

    redirect, get data from db, sessions, validating, hash_pw

--- init.py
+++ init.py
@@ -2,62 +2,79 @@
 from flask import Flask, request
 import pymongo
 from pymongo import MongoClient
+import flask
+from flask import session
+import re
 
-"""
-################################################
+app = Flask(__name__)
 
-# DB-Credentials -> spaeter Config file oder so
-address  = '149.172.144.70' # aktuelle gebe ich dir wenn ich portfreigabe mache
-#address  = 'localhost'
-port     = 27017
-username = None
-password = None
-db_name  = 'gdv'
-#################################################
+######## GLOBALS ########
+db = None
 
+#########################
 
-class DB:
-
-
-    def init( self, addr, port, username, password, db_name ):
-        self.addr     = addr
-        self.port     = port
-        self.username = username
-        self.password = password
-        self.db_name  = db_name
-
-        self.client = MongoClient( address, port )
-"""
 
 def readhtml(filename):
-    data     = open(filename, "r")
-    str      = data.read()
+    data = open(filename, "r")
+    str  = data.read()
     data.close()
     return str
 
-app = Flask(__name__)
+
+def connect_to_db():
+    global db
+    if (db is None):
+        CONNECTION_STRING = 'mongodb://Nagel:xL8NyJYnnKkuBM4WaVz8NVsGTg@192.168.0.90:27018'
+        client            = MongoClient( CONNECTION_STRING )
+        db                = client[ 'sse' ]
+    return db
+
+
+def hash_passwd(text):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if (char.isupper()):
+            result += chr((ord(char) + 1-65) % 26 + 65)
+        else:
+            result += chr((ord(char) + 1 - 97) % 26 + 97)
+    return result
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RThuifwebghweqijfgoew8tfuw2tr1t27&/)"ß9tg04'
+
+######################################################################################
 
 @app.route("/")
 def init():
     return readhtml('login.html')
 
-@app.route("/validate", methods=['POST'])
+@app.route("/validate", methods = ['POST'])
 def validate():
-    name = ""
-    pw   = ""
 
-    if request.method == 'POST':
-        name = request.form[ 'email' ]
-        pw   = request.form[ 'passwd' ]
-        # suche in db nach email und check ob passwort stimmt und rolle
-        # wenn email net da ist kommt meldung zum regisitrieren
-        # wenn ja, dann entscheide nach rolle und leite weiter zu prof oder student
-        return 'validating'
+    db = connect_to_db()
 
+    if request.method=='POST':
+        email   = request.form['email']
+        pw      = hash_passwd( request.form['passwd'] )
+        col     = db['user']
+        find_db = col.find( {'email': email, 'passwd': pw} )
+        for user in find_db:
+            session['username'] = user['username']
+            session['role']     = user['role']
+            if(session['role'] == 'Student'):
+                return flask.redirect("/student")
+            elif(session['role'] == 'Professor'):
+                return flask.redirect("/professor")
+            elif(session['role'] == 'Administrator'):
+                return flask.redirect("/administrator")
+    return flask.redirect("/")
 
 @app.route("/student")
 def student():
-    return readhtml('student.html')
+    datei    = readhtml('student.html')
+    replaced = re.sub('<span id="studentname"></span>', '<span id="studentname">'+session['username']+'</span>', datei)
+    return replaced
 
 
 @app.route("/noteneinsicht")
@@ -68,6 +85,16 @@ def noteneinsicht():
 @app.route("/klausuren")
 def klausuren():
     return readhtml('klausuren.html')
+
+
+@app.route("/professor")
+def klausuren():
+    return readhtml('professor.html')
+
+
+@app.route("/administrator")
+def klausuren():
+    return readhtml('administrator.html')
 
 
 #start
